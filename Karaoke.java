fba1f5450d13951781055c4b4147ae56b83b7f73
Dennis Flannigan <DLFlann@gmail.com>
2017-08-31 19:35:23 -0500

    Add KaraokeMachine class with basic IO funtionality to add songs and quit program

--- Karaoke.java
+++ Karaoke.java
@@ -1,3 +1,4 @@
+import com.dlflann.KaraokeMachine;
 import com.dlflann.model.Song;
 import com.dlflann.model.SongBook;
 
@@ -5,13 +6,8 @@ public class Karaoke
 {
     public static void main(String[] args)
     {
-        Song song = new Song(
-            "Michael Jackson",
-            "Beat It",
-            "https://www.youtube.com/watch?v=Ym0hZG-zNOk");
         SongBook songBook = new SongBook();
-        System.out.printf("Adding %s %n", song);
-        songBook.addSong(song);
-        System.out.printf("There are %d songs. %n", songBook.getSongCount());
+        KaraokeMachine machine = new KaraokeMachine(songBook);
+        machine.run();
     }
 }
